d717b0362e9ef846db4d7df750f1a6f33219ef13
Shurygina Julia <melam.2916@gmail.com>
2024-10-18 15:10:53 +0300

    test:123

--- commit_message_check.sh
+++ commit_message_check.sh
@@ -9,9 +9,9 @@ fi
 # Читаем сообщение коммита из файла
 commit_message=$(cat "$1")
 
-# Проверяем, начинается ли сообщение с "feat:" или "fix:"
-if [[ ! $commit_message =~ ^(feat|fix)\: ]]; then
-  echo "Commit message format is incorrect. It should start with 'feat:' or 'fix:'."
+# Проверяем, начинается ли сообщение с "feat:" или "fix:", и есть ли после этого текст
+if [[ ! $commit_message =~ ^(feat|fix):\ [A-Za-z0-9] ]]; then
+  echo "Commit message format is incorrect. It should start with 'feat:' or 'fix:' followed by a space and a description."
   exit 1
 else
   echo "Commit message format is correct. Proceed with the commit."
