96c7e82d06b0940e8b812ef9f089c93a58af89ca
Shurygina Julia <melam.2916@gmail.com>
2024-10-18 16:14:41 +0300

    123

--- .lefthook/commit-msg/template_checker.sh
+++ .lefthook/commit-msg/template_checker.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
-exit 1
-# Отладочное сообщение для проверки, что скрипт запускается
-echo "Starting commit message check..."
-
-# Читаем сообщение коммита из файла (путь передан как $1)
-commit_message=$(cat "$1")
-
-# Выводим сообщение для отладки
-echo "Commit message is: $commit_message"
-
-# Проверяем формат сообщения: должно начинаться с 'feat:' или 'fix:' с последующим текстом
-if [[ ! $commit_message =~ ^(feat|fix)\ \([A-Z]+-[0-9]+\):\ .+ ]]; then
-  echo "Commit message format is incorrect. It should follow the pattern 'feat (ABCD-1234): commit message' or 'fix (ABCD-1234): commit message'."
+INPUT_FILE=$1
+START_LINE=`head -n1 $INPUT_FILE`
+PATTERN="^(TICKET)-[[:digit:]]+: "
+if ! [[ "$START_LINE" =~ $PATTERN ]]; then
+  echo "Bad commit message, see example: TICKET-123: some text"
   exit 1
-else
-  echo "Commit message format is correct. Proceed with the commit."
 fi
-
-# Отладочное сообщение для проверки, что скрипт завершился
-echo "Commit message check finished."
