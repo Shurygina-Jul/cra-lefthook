59b4b8b696d9a18d0f57fa72e1c2ab9f32ce42a7
Shurygina Julia <melam.2916@gmail.com>
2024-10-18 10:46:25 +0300

    настройка

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.123
+          Edit <code>src/App.tsx</code> and save to reload.123-456
         </p>
         <a
           className="App-link"
