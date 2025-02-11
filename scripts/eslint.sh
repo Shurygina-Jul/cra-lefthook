0719c41f8bbf5b56776377462f0177246257e925
Shurygina Julia <melam.2916@gmail.com>
2025-02-11 15:20:29 +0300

    TICKET-4: настроим линтер

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -8,7 +8,7 @@ fi
 FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E '.js$|.jsx$|.ts$|.tsx$')
 if [ -n "$FILES" ]; then
   echo "Running ESLint on: $FILES"
-  npx eslint $FILES
+  $(npm bin)/eslint $FILES
   ESLINT_EXIT_CODE=$?
   if [ $ESLINT_EXIT_CODE -ne 0 ]; then
     echo "ESLint found errors. Please fix them before committing."
