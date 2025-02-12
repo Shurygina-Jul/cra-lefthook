bf0b209f39a102050595ad6a5a352dc9402f1324
Shurygina Julia <melam.2916@gmail.com>
2025-02-12 15:10:49 +0300

    feat(TICKET-4): настроим линтер и исправим ошибки

---
+++ .lefthook/commit-msg/template_checker.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+console.log("Checking commit message...");
+
+const commitMessage = fs.readFileSync(process.argv[2], "utf8").trim();
+console.log("Текст коммита - ", commitMessage);
+
+const commitPattern =
+  /^(feat|fix|chore|docs|style|refactor|test)([A-Z]+-[0-9]+): .+/;
+
+if (!commitPattern.test(commitMessage)) {
+  console.log(
+    "Commit message format is incorrect. It should be in the format 'type(TICKET-123): description'."
+  );
+
+  process.exit(1);
+}
